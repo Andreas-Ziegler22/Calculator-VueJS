09567d1c97f6f53cb9f4a20f7a09a8239e0bc957
MrBurp <andreass.ziegler@gmail.com>
2023-01-06 15:39:45 +0100

    app style body

--- calculator-vuejs/src/App.vue
+++ calculator-vuejs/src/App.vue
@@ -11,4 +11,24 @@ export default {};
   font-family: "RobotoMono";
   src: url("./fonts/RobotoMono-Thin.ttf");
 }
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
 </style>
