0825d17761737a60b40f26d78de695065a8571e1
MrBurp <andreass.ziegler@gmail.com>
2023-01-06 16:06:03 +0100

    created 2 componets button and display

---
+++ calculator-vuejs/src/main/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from "../components/Button";
+import Display from "../components/Dsiplay";
+
+export default {
+  components: { Button, Display },
+};
+</script>
+
+<style></style>
