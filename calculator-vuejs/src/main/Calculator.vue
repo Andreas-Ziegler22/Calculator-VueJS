6940b1cfc25adb304952c9fb21f6b38712980484
MrBurp <andreass.ziegler@gmail.com>
2023-01-08 17:35:59 +0100

    typing number and clear working

--- calculator-vuejs/src/main/Calculator.vue
+++ calculator-vuejs/src/main/Calculator.vue
@@ -41,7 +41,22 @@ export default {
       Object.assign(this.$data, this.$options.data());
     },
     setOperation(operation) {},
-    addDigit(n) {},
+    addDigit(n) {
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
+      }
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
+
+      if (n !== ".") {
+        const i = this.current;
+        const newValue = parseFloat(displayValue);
+        this.values[i] = newValue;
+      }
+    },
   },
 };
 </script>
